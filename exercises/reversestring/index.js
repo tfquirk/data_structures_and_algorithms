701bc5f7bdfbeb0823dae7c51506806d2c87ff9f
tfquirk <timothyfquirk@gmail.com>
2019-05-20 13:58:49 -0400

    Completed first exercise in series

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // my solution:
+  // let reversedString = "";
+  //
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   reversedString += str[i];
+  // }
+  //
+  // return reversedString;
+
+  // sample solution 1:
+  // let splitString = str.split("");
+  // return splitString.reverse().join("");
+
+  // return str
+  //   .split("")
+  //   .reverse()
+  //   .join("");
+
+  // sample solution 2:
+  // let reversed = "";
+  //
+  // for (let char of str) {
+  //   reversed = char + reversed;
+  // }
+  //
+  // return reversed;
+
+  // sample solution 3:
+  // return str.split("").reduce((reversed, char) => {
+  //   return char + reversed;
+  // }, "");
+
+  return str.split("").reduce((reversed, char) => char + reversed, "");
+}
 
 module.exports = reverse;
