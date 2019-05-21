f3b8f15f26e53e7301fbfeebd2f86a1d1cd5ceee
tfquirk <timothyfquirk@gmail.com>
2019-05-21 11:51:42 -0400

    my solution added. All tests pass

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // my solution:
+  let charHash = {};
+  let mostCommonChar = "";
+  let charCount = 0;
+
+  str.split("").forEach(char => {
+    charHash[char] ? (charHash[char] += 1) : (charHash[char] = 1);
+  });
+
+  for (char in charHash) {
+    if (charHash[char] > charCount) {
+      mostCommonChar = char;
+      charCount = charHash[char];
+    }
+  }
+
+  return mostCommonChar;
+}
 
 module.exports = maxChar;
