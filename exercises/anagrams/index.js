bb69e76987a206b633c838137763cdf8df903a8e
tfquirk <timothyfquirk@gmail.com>
2019-05-22 14:53:37 -0400

    my solution added to anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // my solution:
+  const stringA_cleaned = stringA.replace(/[^\w]/g, "").toLowerCase();
+  const stringB_cleaned = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  const stringA_hash = {};
+  const stringB_hash = {};
+
+  for (let char of stringA_cleaned) {
+    stringA_hash[char] ? (stringA_hash[char] += 1) : (stringA_hash[char] = 1);
+  }
+
+  for (let char of stringB_cleaned) {
+    stringB_hash[char] ? (stringB_hash[char] += 1) : (stringB_hash[char] = 1);
+  }
+
+  for (let key in stringA_hash) {
+    if (stringA_hash[key] !== stringB_hash[key]) {
+      return false;
+    }
+  }
+
+  for (let key in stringB_hash) {
+    if (stringA_hash[key] !== stringB_hash[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
