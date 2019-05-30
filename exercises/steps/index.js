8d621fc8f7438a64df370e9ecc821692b3a6b24e
tfquirk <timothyfquirk@gmail.com>
2019-05-30 10:47:51 -0400

    my solution, suggested solution 1, and recursive solution now included

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,25 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  // suggested solution 1:
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += "#";
-      } else {
-        stair += " ";
-      }
-    }
-    console.log(stair);
+function steps(n, row = 0, stair = "") {
+  // suggested solution 2 (recursive):
+  // first include base case
+  if (n === row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  steps(n, row, stair);
 }
 
 module.exports = steps;
@@ -48,4 +54,17 @@ module.exports = steps;
 //
 // for (let el of steps) {
 //   console.log(el);
+// }
+
+// suggested solution 1:
+// for (let row = 0; row < n; row++) {
+//   let stair = "";
+//   for (let column = 0; column < n; column++) {
+//     if (column <= row) {
+//       stair += "#";
+//     } else {
+//       stair += " ";
+//     }
+//   }
+//   console.log(stair);
 // }
