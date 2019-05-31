e9709d670793f4bf215a88769feccfdd94dbc5cd
tfquirk <timothyfquirk@gmail.com>
2019-05-31 17:12:20 -0400

    first suggested solution to vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,17 +8,28 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-  // my solution:
-  const vowels = ["a", "e", "i", "o", "u"];
-  let vowelCount = 0;
+  let count = 0;
+  const letters = ["a", "e", "i", "o", "u"];
 
-  for (let char of str) {
-    if (vowels.includes(char.toLowerCase())) {
-      vowelCount += 1;
+  for (let char of str.toLowerCase()) {
+    if (letters.includes(char)) {
+      count += 1;
     }
   }
 
-  return vowelCount;
+  return count;
 }
 
 module.exports = vowels;
+
+// // my solution:
+// const vowels = ["a", "e", "i", "o", "u"];
+// let vowelCount = 0;
+//
+// for (let char of str) {
+//   if (vowels.includes(char.toLowerCase())) {
+//     vowelCount += 1;
+//   }
+// }
+//
+// return vowelCount;
